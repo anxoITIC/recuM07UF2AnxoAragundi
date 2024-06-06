fda2c90e1b3dd834fbeadde54b52dab7e6f52b56
anxoaragundi <anxoaragundimesias21@gmail.com>
2024-06-06 17:02:23 +0200

    rutes de admin.php afegides

--- M7_UF2/routes/admin.php
+++ M7_UF2/routes/admin.php
@@ -8,20 +8,17 @@ Route::middleware(['admin_db'])->group(function(){
     //els names de cada ruta tenen el mateix nom que el mètode al que fan referència
     
     //ruta 1: demana les dades de centre amb path '/centres'
-   
+    Route::get('/centres', [AdminController::class, 'centres'])->name('centres');
     //ruta 2: envia dades d'usuari per fer el login/signin amb path '/usuaris'
-    
+    Route::post('/usuaris', [AdminController::class, 'users'])->name('usuaris');
     //ruta3: demana dades de professorat amb path '/professorat'
-   
+    Route::get('/professorat', [AdminController::class, 'teach'])->name('teachers');
     //ruta4: demana dades d'alumnat amb path '/alumnat'
-    
+    Route::get('/alumnat', [AdminController::class, 'stud'])->name('students');
     //ruta5: demana mostrar la view d'admin amb path '/admin_view'
-    
+    Route::get('/admin_view', [AdminController::class, 'v_admin'])->name('admin_view');
 
 });
 
     
-
-
-
 ?>
